7f7bef9a25a18eb76d4f25c8e1337adffc4c6d4e
Anugya Jaiswal <139376690+anugyajais@users.noreply.github.com>
2024-12-31 15:07:53 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (46.38%) - LeetSync

---
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+
+        unordered_set<int> travelDays(days.begin(), days.end());
+        int lastDay = days.back();
+        vector<int> dp(lastDay + 1, INT_MAX);
+        dp[0] = 0;
+        for (int i = 1; i <= lastDay; ++i) {
+            if (travelDays.find(i) == travelDays.end()) {
+                dp[i] = dp[i - 1];
+            } else {
+                dp[i] = min({dp[i - 1] + costs[0], dp[max(0, i - 7)] + costs[1],
+                             dp[max(0, i - 30)] + costs[2]});
+            }
+        }
+        return dp[lastDay];
+    }
+};
