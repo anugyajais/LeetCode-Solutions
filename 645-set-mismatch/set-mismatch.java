f20e7b5806625419ca2974a70ea4d5c874ab5888
Anugya Jaiswal <139376690+anugyajais@users.noreply.github.com>
2025-07-05 23:21:33 +0530

    Time: 3 ms (78.66%) | Memory: 46 MB (25.90%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+
+        int n = nums.length;
+        int i = 0;
+        while (i < n) {
+            int correctIndex = nums[i] - 1;
+            if (nums[i] != nums[correctIndex]) {
+                int t = nums[i];
+                nums[i] = nums[correctIndex];
+                nums[correctIndex] = t;
+            } else {
+                i++;
+            }
+        }
+        for (i = 0; i < n; i++) {
+            if (nums[i] != (i + 1)) {
+                return new int[]{ nums[i], i+1}; 
+
+            }
+        }
+        return new int[] {-1,-1};
+    }
+}
